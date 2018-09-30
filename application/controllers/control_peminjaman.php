84cafee0df92fa3aea6ae6a7a1a6c967b350c0a3
inoxxe <gochrome007@gmail.com>
2018-09-30 19:28:15 +0700

    pembenaran

--- application/controllers/control_peminjaman.php
+++ application/controllers/control_peminjaman.php
@@ -23,6 +23,7 @@ class Control_peminjaman extends CI_Controller {
 					$this->load->view('helloworld/index');
 				}
 				
+				
 			}
 	
 
@@ -37,7 +38,7 @@ class Control_peminjaman extends CI_Controller {
 				$data = $this->model_peminjaman->GetWhere('register',$where);
 				$data = array('data' => $data );	
 				$this->load->view('profil',$data);
-				
+
 			}else{
 					$this->load->view('helloworld/index');
 
